44664032f2b211a0342e637b37da231e4f937caa
Federico Mancini <fredrik.leftin@gmail.com>
2025-06-27 02:44:29 +0200

    v1.14 - build test

--- cloud_run/mytest.py
+++ cloud_run/mytest.py
@@ -1,9 +1,13 @@
 import json, time, asyncio, datetime
+print("IMPORT 1 OK")
 import utils.vertexai_utils as vxc
+print("IMPORT 2 OK")
 import utils.gcs_utils as gcs
-
+print("IMPORT 3 OK")
 from google.cloud import storage
+print("IMPORT 4 OK")
 from utils.cache_utils import alert_hash, load_alert_cache, save_alert_cache
+print("IMPORT 5 OK")
 
 def mytest():
     print("test")
