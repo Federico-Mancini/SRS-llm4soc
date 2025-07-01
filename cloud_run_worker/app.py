57d2c7b2b7b30184cec871db514810aa0a66cdd3
Federico Mancini <fredrik.leftin@gmail.com>
2025-07-01 20:05:41 +0200

    v8.6 - hotfix

--- cloud_run_worker/app.py
+++ cloud_run_worker/app.py
@@ -42,7 +42,7 @@ async def run_batch(request: Request):
 
     # Classificazione alert del batch
     #batch_result_list = analyze_batch_sync(batch_df, batch_id)
-    batch_result_list = analyze_batch_async_with_cache(batch_df, batch_id)
+    batch_result_list = await analyze_batch_async_with_cache(batch_df, batch_id)
     #batch_result_list = await analyze_batch_async(batch_df, batch_id)   # operazione sincrona (attendo che tutti i task asyncio terminino)
 
     # Salvataggio risultati su GCS
